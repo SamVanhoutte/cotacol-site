f33facda448435b5d82a124ab0e8c891acb8ee8e
Sam <sam.vanhoutte@marchitec.be>
2021-01-22 06:52:18 +0100

    Add OAuth

--- src/CotacolApp/Startup.cs
+++ src/CotacolApp/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CotacolApp.Areas.Identity;
 using CotacolApp.Data;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace CotacolApp
 {
@@ -38,6 +39,47 @@ namespace CotacolApp
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            
+            // Strava authentication
+             services.AddAuthentication().AddOAuth("Strava",
+                oAuthOptions =>
+                {
+                    //http://www.strava.com/oauth/authorize?client_id=4987&response_type=code&redirect_uri=https://cotacol-hunting-workers.azurewebsites.net/api/StravaTokenHttpTrigger&approval_prompt=force&state=cotacolll&scope=read,read_all,activity:read_all,activity:read,activity:write,profile:write
+                    //https://www.strava.com/oauth/authorize?client_id=54778&scope=read&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A5001%2Fsignin-strava&state=CfDJ8JoKNYGkoJRGiyjnLkJiaUXjUWR4qUReuvnpsCJPXslZ-CQ8zd_K_29IzJFzt3FOh_ARdAIChh1omQs_e-sTuGf06AgozflcyVSbLRtr4kenuHkd0i-TDE0OglQgZWCw4OdZ3D6flAsl1SvtBpZCUPa9h6OXn50HlcpZmnsz_LnoyLxviJlLkCU2Iipq9iWsZ6ze0kQLXgGSZF7yPZU_Xw0nGpPtelrgKW-az60jJJao-T9CdrEBBTcX-8TwPF-6kQrySYHmyvWfzxsTVBr7dp-bq4QGcF2-pZTdRtkym2SRwVYWQi9H4WNZW3zy8VMq1Q
+                    oAuthOptions.ClientId = "54778"; //Configuration["strava-app-id"];
+                    oAuthOptions.ClientSecret =
+                        "29dec80b378c4cb04be78d1defba2a7458b72b26"; //Configuration["strava-app-secret"];
+                    oAuthOptions.Scope.Clear();
+                    oAuthOptions.Scope.Add(
+                        "read,read_all,activity:read_all,activity:read,activity:write,profile:write");
+                    oAuthOptions.CallbackPath = "/cols"; //"/signin-strava";
+                    oAuthOptions.AuthorizationEndpoint = "https://www.strava.com/oauth/authorize";
+                    oAuthOptions.TokenEndpoint = "https://www.strava.com/api/v3/oauth/token";
+                    //oAuthOptions.SignInScheme = IdentityConstants.ExternalScheme;
+                    oAuthOptions.Events = new OAuthEvents()
+                    {
+                        OnRemoteFailure = loginFailureHandler =>
+                        {
+                            Console.WriteLine("Remote Error");
+                            Console.WriteLine(loginFailureHandler.Failure.Message);
+                            var authProperties =
+                                oAuthOptions.StateDataFormat.Unprotect(loginFailureHandler.Request.Query["state"]);
+                            loginFailureHandler.Response.Redirect("/Identity/Account/Login");
+                            loginFailureHandler.HandleResponse();
+                            return Task.FromResult(0);
+                        }, 
+                        OnAccessDenied = handler =>
+                        {
+                            Console.WriteLine(handler.Response.StatusCode);
+                            return Task.FromResult(0);
+                        }
+                    };
+                    oAuthOptions.Validate();
+                });
+            
+            
+            
+            // Dependency injectoin
             services
                 .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>
                 >();
