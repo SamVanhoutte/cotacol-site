3143e944589481169e56a015770d1bc485b59a9a
Sam <sam.vanhoutte@marchitec.be>
2021-02-09 20:36:12 +0100

    map fixes

--- src/CotacolApp/Services/MapService.cs
+++ src/CotacolApp/Services/MapService.cs
@@ -4,11 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Columbae;
 using CotacolApp.Models;
+using CotacolApp.Models.CotacolApi;
 using GoogleMapsComponents;
 using GoogleMapsComponents.Maps;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Manage.Internal;
 using Microsoft.JSInterop;
 using Serilog.Core;
+using Polyline = GoogleMapsComponents.Maps.Polyline;
 
 namespace CotacolApp.Services
 {
@@ -57,14 +61,14 @@ namespace CotacolApp.Services
         }
 
         public async Task ShowClimbAsync(Map map1, IJSRuntime jsRuntime, UserClimb climb,
-            bool plotTrack = true, bool singleClimb = false)
+            bool plotTrack = true, bool singleClimb = false, bool showArrows = false)
         {
-            
             if (!string.IsNullOrEmpty(climb.Polyline))
             {
                 var polyline = Columbae.Polyline.ParsePolyline(climb.Polyline);
-                await AddMarkerAsync(map1, jsRuntime, climb, polyline, singleClimb);
-                if (plotTrack) await DrawPolylineAsync(map1, jsRuntime, polyline);
+
+                if (!plotTrack) await AddMarkerAsync(map1, jsRuntime, climb, polyline, singleClimb);
+                if (plotTrack) await DrawPolylineAsync(map1, jsRuntime, climb, polyline, true);
                 if (singleClimb) await ZoomToClimbAsync(map1, polyline);
             }
         }
@@ -94,6 +98,16 @@ namespace CotacolApp.Services
                 await marker.SetAnimation(Animation.Drop);
             }
 
+            var infoWindow = await CreateInfoWindowAsync(climb, jsRuntime, start);
+            await marker.AddListener("click", async () =>
+            {
+                await infoWindow.Open(map1);
+            });
+            _markers.Add(marker);
+        }
+
+        private async Task<InfoWindow> CreateInfoWindowAsync(ClimbData climb,IJSRuntime jsRuntime, Polypoint position)
+        {
             var infoWindowContent = $@"<table class='table table-striped table-dark'><tbody>
                         <tr>
                         <td colspan=2>{climb.Name}</td>
@@ -114,19 +128,14 @@ namespace CotacolApp.Services
                         </table>";
             var infoWindow = await InfoWindow.CreateAsync(jsRuntime, new InfoWindowOptions()
             {
-                Position = new LatLngLiteral(start.Longitude, start.Latitude)
+                Position = new LatLngLiteral(position.Longitude, position.Latitude)
             });
-            await marker.AddListener("click", async () =>
-            {
-                await infoWindow.SetContent(infoWindowContent);
-                await infoWindow.SetPosition(new LatLngLiteral(start.Longitude, start.Latitude));
-                await infoWindow.Open(map1);
-            });
-            _markers.Add(marker);
+            await infoWindow.SetContent(infoWindowContent);
+            await infoWindow.SetPosition(new LatLngLiteral(position.Longitude, position.Latitude));
+            return infoWindow;
         }
-
-
-        private async Task DrawPolylineAsync(Map map1, IJSRuntime jsRuntime, Columbae.Polyline polyline)
+        private async Task DrawPolylineAsync(Map map1, IJSRuntime jsRuntime, ClimbData climb, Columbae.Polyline polyline,
+            bool showArrow = false)
         {
             var path = polyline.Vertices.Select(v => new LatLngLiteral(v.Longitude, v.Latitude));
             var line = await Polyline.CreateAsync(jsRuntime, new PolylineOptions()
@@ -137,20 +146,60 @@ namespace CotacolApp.Services
                 Editable = false,
                 Map = map1, Path = path
             });
+            if (showArrow)
+            {
+                await line.SetOptions(new PolylineOptions
+                {
+                    Icons = new[]
+                    {
+                        new IconSequence
+                        {
+                            Icon = new Symbol {Path = SymbolPath.CIRCLE, Scale = 3f, StrokeColor = "green"},
+                            Offset = "0%"
+                        },
+                        new IconSequence
+                        {
+                            Icon = new Symbol {Path = SymbolPath.CIRCLE, Scale = 3f, StrokeColor = "red"},
+                            Offset = "100%"
+                        },
+                        // new IconSequence {Icon = new Symbol {Path = SymbolPath.FORWARD_OPEN_ARROW}, Offset = "80%"},
+                        // new IconSequence
+                        // {
+                        //     Icon = new Symbol {Path = SymbolPath.FORWARD_OPEN_ARROW, Scale = 3f},
+                        //     Offset = "50%"
+                        // },
+                        // new IconSequence {Icon = new Symbol {Path = SymbolPath.FORWARD_OPEN_ARROW}, Offset = "40%"},
+                        // new IconSequence {Icon = new Symbol {Path = SymbolPath.FORWARD_OPEN_ARROW}, Offset = "20%"},
+                    }
+                });
+                var infoWindow = await CreateInfoWindowAsync(climb, jsRuntime, polyline.Vertices.First());
+                await line.AddListener("click", async () =>
+                {
+                    await infoWindow.Open(map1);
+                });
+            }
+
             _lines.Add(line);
         }
 
         public async Task ClearClimbsAsync()
         {
-            var tasks = new List<Task> { };
-            tasks.AddRange(
-                _markers.Select(marker => marker.SetMap(null)));
-            await Task.WhenAll(tasks);
+            if (_markers != null)
+            {
+                var tasks = new List<Task> { };
+                tasks.AddRange(
+                    _markers.Select(marker => marker.SetMap(null)));
+                await Task.WhenAll(tasks);
+            }
 
-            tasks = new List<Task> { };
-            tasks.AddRange(
-                _lines.Select(line => line.SetMap(null)));
-            await Task.WhenAll(tasks);
+            if (_lines != null)
+            {
+                var tasks = new List<Task> { };
+                tasks.AddRange(
+                    _lines.Select(line => line.SetMap(null)));
+                await Task.WhenAll(tasks);
+            }
+
             if (_markerClustering != null)
             {
                 await _markerClustering.ClearMarkers();
