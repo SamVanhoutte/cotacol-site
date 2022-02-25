c5c842d98da0d7062edb8a6b09da70aa05ed42a9
samvanhoutte <sam.vanhoutte@marchitec.be>
2022-02-25 14:27:18 +0100

    Add min-max date to BadgeOfMonth

--- src/CotacolApp/Models/CotacolApi/BadgeOfMonthData.cs
+++ src/CotacolApp/Models/CotacolApi/BadgeOfMonthData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -5,23 +6,20 @@ namespace CotacolApp.Models.CotacolApi
 {
     public class BadgeOfMonthData
     {
-        [JsonProperty("badgeType")]
-        public string BadgeType { get; set; }
+        [JsonProperty("badgeType")] public string BadgeType { get; set; }
 
-        [JsonProperty("year")]
-        public int Year { get; set; }
+        [JsonProperty("year")] public int Year { get; set; }
 
-        [JsonProperty("month")]
-        public int Month { get; set; }
+        [JsonProperty("month")] public int Month { get; set; }
+        [JsonProperty("startTime")] public DateTime? StartTime { get; set; }
+        [JsonProperty("endTime")] public DateTime? EndTime { get; set; }
 
-        [JsonProperty("badgeName")]
-        public string BadgeName { get; set; }
 
-        [JsonProperty("badgeDescription")]
-        public string BadgeDescription { get; set; }
+        [JsonProperty("badgeName")] public string BadgeName { get; set; }
 
-        [JsonProperty("cotacolIds")]
-        public List<int> CotacolIds { get; set; }
+        [JsonProperty("badgeDescription")] public string BadgeDescription { get; set; }
+
+        [JsonProperty("cotacolIds")] public List<int> CotacolIds { get; set; }
 
         public string MonthKey => $"{Year}-{Month:00}";
     }
