8e41bd6232fa0fe94697c105652609edf8a9448a
samvanhoutte <sam.vanhoutte@marchitec.be>
2022-03-08 14:11:34 +0100

    Update client to meet new strava segment response

--- src/CotacolApp/Models/CotacolApi/StravaSegmentResponse.cs
+++ src/CotacolApp/Models/CotacolApi/StravaSegmentResponse.cs
@@ -2,27 +2,5 @@ using Newtonsoft.Json;
 
 namespace CotacolApp.Models.CotacolApi
 {
-    public  class StravaSegmentResponse
-    {
-        [JsonProperty("segment")]
-        public StravaSegment Segment { get; set; }
 
-        [JsonProperty("statusCode")]
-        public string StatusCode { get; set; }
-
-        [JsonProperty("shouldRetry")]
-        public bool ShouldRetry { get; set; }
-
-        [JsonProperty("succeeded")]
-        public bool Succeeded { get; set; }
-
-        [JsonProperty("retryTimespan")]
-        public string RetryTimespan { get; set; }
-
-        [JsonProperty("rateUsageString")]
-        public string RateUsageString { get; set; }
-
-        [JsonProperty("errorMessage")]
-        public string ErrorMessage { get; set; }
-    }
 }
