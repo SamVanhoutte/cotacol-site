db3a8ae504e02233bf9ce5a4e465a18bdace599d
samvanhoutte <sam.vanhoutte@marchitec.be>
2023-03-14 11:25:49 +0100

    Finished stats pages

--- src/Cotacol.Website/Models/CotacolApi/CotacolClimb.cs
+++ src/Cotacol.Website/Models/CotacolApi/CotacolClimb.cs
@@ -7,5 +7,22 @@ namespace Cotacol.Website.Models.CotacolApi
         public int UniqueUsers { get; set; }
         public int TotalAttempts { get; set; }
         public List<LocalLegend> LocalLegends { get; set; }
+
+        public int MaxAttempts
+        {
+            get
+            {
+                try
+                {
+                    if (LocalLegends == null || !LocalLegends.Any()) return 0;
+                    return LocalLegends.Max(ll => ll.Attempts);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return 0;
+                }
+            }
+        }
     }
 }
