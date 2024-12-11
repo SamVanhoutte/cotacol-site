163ea28c49c2271640724f396594e02d57a8b1c0
Sam Vanhoutte <sam.vanhoutte@marchitec.be>
2024-12-11 12:04:51 +0100

    include screenshots

--- tests/playwright/cotacol/cotacol-public.spec.ts
+++ tests/playwright/cotacol/cotacol-public.spec.ts
@@ -1,61 +1,61 @@
 import { test, expect, type Page, TestInfo } from '@playwright/test';
 
 test('Verify page title', async ({ page }, testInfo) => {
-  await page.goto('/stats');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Cotacol.cc");
+  await screenshotPage(page, testInfo, 'home');
+});
+
+test('Col stats should return all cols', async ({ page }, testInfo) => {
+  await page.goto('/colstats');
+
+  // Count the number of elements with an id starting with 'cotacol'
+  await page.waitForSelector('[id="cotacolitem"]', { timeout: 10000 });
+  const cotacolElements = await page.locator('[id="cotacolitem"]').count();
+  expect(cotacolElements).toBe(1000); // Verify the count is 1000
+  await screenshotPage(page, testInfo, 'colstats');
+});
+
+test('Leaderboard exists', async ({ page }, testInfo) => {
+  await page.goto('/stats');
+
+  // Count the number of elements with an id starting with 'cotacol'
+  await page.waitForSelector('[id="cotacoluser"]', { timeout: 10000 });
+  const cotacolElements = await page.locator('[id="cotacoluser"]').count();
+  expect(cotacolElements).toBeGreaterThan(50); // Verify the count is 1000
   await screenshotPage(page, testInfo, 'stats');
 });
 
-// test('Col stats should return all cols', async ({ page }) => {
-//   await page.goto('/colstats');
+test('Climbs list should work', async ({ page }, testInfo) => {
+  await page.goto('/climbs');
 
-//   // Count the number of elements with an id starting with 'cotacol'
-//   await page.waitForSelector('[id="cotacolitem"]', { timeout: 10000 });
-//   const cotacolElements = await page.locator('[id="cotacolitem"]').count();
-//   expect(cotacolElements).toBe(1000); // Verify the count is 1000
-//   await page.screenshot({ path: 'screens/colstats.png' });
-// });
+  // Count the number of elements with an id starting with 'cotacol'
+  const cotacolElements = await page.locator('[id^="cotacol"]').count();
+  expect(cotacolElements).toBe(1000); // Verify the count is 1000
 
-// test('Leaderboard exists', async ({ page }) => {
-//   await page.goto('/stats');
+  // Generate a random number between 1 and 1000
+  const randomNumber = Math.floor(Math.random() * 1000) + 1;
+  await screenshotPage(page, testInfo, 'climbs');
 
-//   // Count the number of elements with an id starting with 'cotacol'
-//   await page.waitForSelector('[id="cotacoluser"]', { timeout: 10000 });
-//   const cotacolElements = await page.locator('[id="cotacoluser"]').count();
-//   expect(cotacolElements).toBeGreaterThan(50); // Verify the count is 1000
-//   await page.screenshot({ path: 'stats.png' });
-// });
+  // Click the element with id 'cotacol-{randomnumber}'
+  await page.locator(`#cotacol-${randomNumber}`).click();
+});
 
-// test('Climbs list should work', async ({ page }) => {
-//   await page.goto('/climbs');
+test('Col list to random detail', async ({ page }, testInfo) => {
+  const cotacolId = Math.floor(Math.random() * 1000) + 1;
+  await page.goto(`/cotacol/${cotacolId}`);
+  const elevationElement = page.locator('#cotacolelevation');
+  await expect(page.locator('#cotacolelevation')).toBeVisible(); // Assert that the element is visible
+  await expect(page.locator('#cotacolgrade')).toBeVisible(); // Assert that the element is visible
+  await expect(page.locator('#cotacoldistance')).toBeVisible(); // Assert that the element is visible
+  await expect(page.locator('#cotacolsurface')).toBeVisible(); // Assert that the element is visible
+  await expect(page.locator('#cotacoldone')).toHaveCount(0); // Assert that the element is visible
+  await expect(page.locator('#map1')).toBeVisible(); // Assert that the element is visible
+  await screenshotPage(page, testInfo, 'climbdetail');
 
-//   // Count the number of elements with an id starting with 'cotacol'
-//   const cotacolElements = await page.locator('[id^="cotacol"]').count();
-//   expect(cotacolElements).toBe(1000); // Verify the count is 1000
-
-//   // Generate a random number between 1 and 1000
-//   const randomNumber = Math.floor(Math.random() * 1000) + 1;
-//   await page.screenshot({ path: 'screens/climbs.png' });
-
-//   // Click the element with id 'cotacol-{randomnumber}'
-//   await page.locator(`#cotacol-${randomNumber}`).click();
-// });
-
-// test('Col list to random detail', async ({ page }) => {
-//   const cotacolId = Math.floor(Math.random() * 1000) + 1;
-//   await page.goto(`/cotacol/${cotacolId}`);
-//   const elevationElement = page.locator('#cotacolelevation');
-//   await expect(page.locator('#cotacolelevation')).toBeVisible(); // Assert that the element is visible
-//   await expect(page.locator('#cotacolgrade')).toBeVisible(); // Assert that the element is visible
-//   await expect(page.locator('#cotacoldistance')).toBeVisible(); // Assert that the element is visible
-//   await expect(page.locator('#cotacolsurface')).toBeVisible(); // Assert that the element is visible
-//   await expect(page.locator('#cotacoldone')).toHaveCount(0); // Assert that the element is visible
-//   await expect(page.locator('#map1')).toBeVisible(); // Assert that the element is visible
-//   await page.screenshot({ path: 'screens/climbdetail.png' });
-
-// });
+});
 
 
 async function screenshotPage(page: Page, testInfo: TestInfo, name: string) {
