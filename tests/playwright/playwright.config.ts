90ccc067edac5e1eef82c0cff4a2128f932b94a2
Sam Vanhoutte <sam.vanhoutte@marchitec.be>
2024-12-13 13:30:09 +0100

    base url

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig<{}, {}> = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://localhost:7259/', //'https://www.cotacol.cc/',
+    baseURL: 'https://www.cotacol.cc/', //'https://localhost:7259/', //'https://www.cotacol.cc/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
