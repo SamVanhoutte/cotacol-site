6e26eefe960e2a087edb03240ca25c1db42210de
Sam Vanhoutte <sam.vanhoutte@marchitec.be>
2024-12-26 16:30:16 +0100

    update support cases

--- src/Cotacol.Website/Models/Support/StravaActivitiesResult.cs
+++ src/Cotacol.Website/Models/Support/StravaActivitiesResult.cs
@@ -6,6 +6,7 @@ public class StravaActivitiesResult
 {
     public int StravaActivityCount { get; set; }
     public int MissingActivityCount { get; set; }
-    public StravaActivitiesResponse ActivitySummary { get; set; }
     public string Error { get; set; }
+    public List<ActivityDetail> ExistingActivities { get; set; }
+    public List<ActivityDetail> NewActivities { get; set; }
 }
